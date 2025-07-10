dff24f02cb5e4c4fad891392f4b3930f2c965cb5
TERENCE <ashybelt@gmail.com>
2025-07-10 17:44:56 +0800

    撈取 Customer 清單

---
+++ Website/Controllers/CustomerController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using Service.Interface;
+
+using Website.Models.Customer;
+
+namespace Website.Controllers;
+public class CustomerController : Controller
+{
+    private readonly ICustomerService _customService;
+    public CustomerController(ICustomerService customService)
+    {
+        _customService = customService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var data = await _customService.GetListAsync();
+        var model = new List<CustomerViewModel>();
+        if (data != null)
+        {
+            foreach (var item in data)
+            {
+                model.Add(new CustomerViewModel
+                {
+                    CustomerId = item.CustomerId,
+                    CompanyName = item.CompanyName,
+                    ContactName = item.ContactName,
+                    Country = item.Country,
+                    Phone = item.Phone,
+                });
+            }
+        }
+        
+        return View(model);
+    }
+}
---
+++ Website/Models/Customer/CustomerViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Website.Models.Customer;
+
+public class CustomerViewModel
+{
+    [Display(Name = "CustomerId")]
+    public string CustomerId { get; set; } = null!;
+
+    [Display(Name = "CompanyName")]
+    public string CompanyName { get; set; } = null!;
+
+    [Display(Name = "ContactName")]
+    public string? ContactName { get; set; }
+
+    [Display(Name = "Country")]
+    public string? Country { get; set; }
+
+    [Display(Name = "Phone")]
+    public string? Phone { get; set; }
+}
